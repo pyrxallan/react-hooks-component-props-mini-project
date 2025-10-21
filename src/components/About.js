59b9976ee514fa9467e83cab19d84208c8fbb1b8
Allan Ratemo <allanisratemo@gmail.com>
2025-10-21 19:09:36 +0300

    Add Header and About

---
+++ src/components/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function About() {
+  return (
+    <aside>
+      <img
+        src="https://via.placeholder.com/215"
+        alt="blog logo"
+        width="215"
+      />
+      <p>
+        Personal blog by
+        <strong> Dan Abramov</strong>. I explain with words and code.
+      </p>
+    </aside>
+  );
+}
+
+export default About;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+        <h1>Overreacted</h1>
+    </header>
+  );
+}
+
+export default Header;
